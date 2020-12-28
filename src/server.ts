e6a27da2ed5be885e90e3ac25bb68bbfe9adecdc
Magno Vianna <mvianna@live.com>
2020-12-28 00:22:07 -0300

    docker-compose adicionado ao projeto

--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,7 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Server up port 33333, press Ctrl + c, to stop the server');
+  console.log(
+    '[[[[ Server up port 33333, press Ctrl + c, to stop the server ]]]]]',
+  );
 });
